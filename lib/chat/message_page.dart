179d48eb7c717cdd343d5682c31e97efe77e0981
liguoying <15510224541@163com>
2019-12-05 13:52:31 +0800

    chat page

--- lib/chat/message_page.dart
+++ lib/chat/message_page.dart
@@ -1,19 +1,22 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './message_item.dart';
+import './message_data.dart';
 
-class MessagePage extends StatelessWidget{
+class MessagePage extends StatefulWidget {
+  @override
+  _MessagePageState createState() => _MessagePageState();
+}
+
+class _MessagePageState extends State<MessagePage> {
   @override
   Widget build(BuildContext context) {
-
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: AppBar(
-          title: Text("Messages"),
-        ),
-      ),
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: messageData.length,
+          itemBuilder: (BuildContext context, int index) {
+            return MessageItem(messageData[index]);
+          }),
     );
   }
-
 }
