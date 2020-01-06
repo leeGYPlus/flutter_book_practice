fbb73987e4defa7ea0b696981927220e2c99d950
liguoying <15510224541@163com>
2020-01-06 15:16:44 +0800

    Dart特性运用：改造三（方法的改写）

--- lib/dart/four.dart
+++ lib/dart/four.dart
@@ -9,6 +9,8 @@ class Mate {
 
 class Item extends Mate {
   Item(String name, price) : super(name, price);
+  // 重载 + 运算符
+  Item operator +(Item item) => Item(name + item.name,price + item.price);
 }
 
 class ShopCart extends Mate {
@@ -17,13 +19,15 @@ class ShopCart extends Mate {
   List<Item> bookings;
 
   // 将原来的 price() 方法改造成 price 属性的 get 方法
-  double get price {
-    double sum = 0.0;
-    for (var item in bookings) {
-      sum += item.price;
-    }
-    return sum;
-  }
+//  double get price {
+//    double sum = 0.0;
+//    for (var item in bookings) {
+//      sum += item.price;
+//    }
+//    return sum;
+//  }
+  //reduce 的参数为函数
+  double get price => bookings.reduce((value, element)=>value + element).price;
 
 
   // 在真正执行构造函数之前可以对成员变量进行赋值
@@ -45,6 +49,6 @@ class ShopCart extends Mate {
 
 main() {
   ShopCart shopCart = new ShopCart('Mike', '235434453');
-  shopCart.bookings = [Item('apple', 12), Item('banana', 34)];
+  shopCart.bookings = [Item('apple', 12.0), Item('banana', 34.0)];
   print('${shopCart.getInfo()}');
 }
